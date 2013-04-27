0d79ccfd687a0f6bdddeaf0a22086c2c7fc5636f
basilboli <basilboli@gmail.com>
2013-04-27 19:00:36 +0200

    added confirmation logic, simple login/logout,

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 grandvadrouille
 ===============
 
-Little service to visualize roommates contribution to weekly cleaning.
+# What is it ?
+Super simple and intuitive service to manage and visualize roommates contribution to cleaning.
 
-Liberté, égalité, fraternité rocks!
+**Choose*** a team, **confirm*** and **see*** actual cleaning contribution by roommate.
+
+# What is it not ?
+cleaning planning using calendars.
+
+# How to deploy?
+You should have mongodb installed. See also requirements.txt for more details.
+
+    
+0. Go to mongodb shell and add as many users as you want by executing the following command.
+    
+    db.users.insert({"name":"ROOMATE_NAME","score":0})
+for ex. 
+    db.users.insert({"name":"John","score":0})
+    db.users.insert({"name":"Samantha","score":0})
+    db.users.insert({"name":"Bob","score":0})        
+
+property score is actual roommate total contribution and is used for ranking roommates.
+
+1. Launch service using built-in server: 
+    python grandvadrouille.py
+
+2. Go to [http://127.0.0.1:500](http://127.0.0.1:500) and enjoy!
+
+You can also deploy using smth. like gunicorn etc. To be covered later.    
+
+
--- grandvadrouille.py
+++ grandvadrouille.py
@@ -9,8 +9,9 @@
 """
 from __future__ import with_statement
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, _app_ctx_stack
+     render_template, flash, _app_ctx_stack,session
 from flask.ext.pymongo import PyMongo
+from bson.objectid import ObjectId
 import datetime
 
 # configuration
@@ -20,6 +21,7 @@ USERNAME = 'admin'
 PASSWORD = 'admin'
 
 app = Flask(__name__)
+app.secret_key = 'ababagalamaga'
 mongo = PyMongo(app)
 
 @app.route('/test')
@@ -29,12 +31,36 @@ def test():
     for user in users:
         print user['name']
     print 'mongo : ', users
-    return render_template('index.html')
+    return render_template('poc.html')
 
 @app.route('/')
 def home():
+    print 'Current session : ', session
     users = mongo.db.users.find()
     return render_template('index.html',users=users)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    if 'user_id' in session:
+        del session['user_id']
+    return redirect("/")
+
+@app.route('/auth',methods=['POST'])
+def auth():
+    email = request.form["email"]
+    password = request.form['password']
+    if password == 'ababagalamaga':
+        session['user_id'] = email
+        users = mongo.db.users.find()
+        return render_template('index.html',users=users)
+    else:
+        return redirect('/login')
+
+
 
 @app.route('/add',methods=['POST'])
 def addnewcleaning():
@@ -44,25 +70,13 @@ def addnewcleaning():
 
     count = len(team)
     if (count!=0):
-        contribution  = 2.0 / count
-        print 'team count',count
-        print 'contribution : ',contribution
-
-        #updating scores
-        for user_name in team:
-            user = mongo.db.users.find_one({'name':user_name})
-            new_score = user['score'] + contribution
-            mongo.db.users.update({'name':user['name']},{'$set':{"score":new_score}})
-            print 'adding controbution {0} to user {1}: '.format(contribution,user['name'])
-
-        #updating history
+        #adding new item
         now = datetime.datetime.now()
-        new_history_item = dict (date=now,team=','.join(team))
+        new_history_item = dict (date=now,team=team,status='pending')
         mongo.db.history.insert(new_history_item)
-        print 'addnewcleaning {0}+{1}'.format(now,team)
+        print 'add new cleaning {0}+{1}'.format(now,team)
 
-
-    return redirect("/")
+    return redirect("/viz")
 
 
 @app.route('/viz')
@@ -73,8 +87,45 @@ def viz():
 
 @app.route('/history')
 def history():
-    history_items = mongo.db.history.find()
+    history_items = mongo.db.history.find({'status':'confirmed'})#requesting only confirmed items
     return render_template('history.html',history_items=history_items)
+
+@app.route('/pending')
+def pending():
+    """ view cleanings in pending status"""
+    print 'session : ', session
+    if session['admin']==True:
+        pending_items = mongo.db.history.find({'status':'pending'})#requesting only pending items
+        return render_template('pending.html',history_items=pending_items)
+    else:
+        abort(404)
+
+
+@app.route('/confirm',methods=['POST'])
+def confirm():
+    """confirm cleaning"""
+    id  = request.form ['id']
+    if id is None:
+        abort(404)
+
+    cleaning = mongo.db.history.find_one({'_id': ObjectId(id)})
+
+    count = len(cleaning['team'])
+    contribution  = 2.0 / count
+    print 'team count',count
+    print 'contribution : ',contribution
+
+    #updating users scores
+    for user_name in cleaning['team']:
+        user = mongo.db.users.find_one({'name':user_name})
+        new_score = user['score'] + contribution
+        mongo.db.users.update({'name':user['name']},{'$set':{"score":new_score}})
+        print 'adding contribution {0} to user {1}: '.format(contribution,user['name'])
+
+    #pending status -> confirmed
+    cleaning = mongo.db.history.update({'_id': ObjectId(id)},{'$set':{"status":'confirmed'}})
+    return redirect('/pending')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',debug=True)
